8df145ee63cf0cf57e7868bd6be41b5614bc14c8
telamonian <telamonian@users.noreply.github.com>
2022-10-21 10:02:32 -0400

    started adding code to actually consume jupyter settings in App

--- src/widget/condaStoreWidget.tsx
+++ src/widget/condaStoreWidget.tsx
@@ -1,15 +1,31 @@
-import { ReactWidget } from '@jupyterlab/apputils';
 import * as React from 'react';
 
-import {AppExample} from "@conda-store/conda-store-ui"
+import { App } from "@conda-store/conda-store-ui"
+import { ReactWidget } from '@jupyterlab/apputils';
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
 
 export class CondaStoreWidget extends ReactWidget {
+  constructor(settings: ISettingRegistry.ISettings) {
+    super();
+    this.settings = settings;
+  }
 
   render(): JSX.Element {
+    // return (
+    //   <App initState={
+    //     setter => {
+    //       this.settings.changed.connect(() => setter(this.settings.composite));
+    //       setter(this.settings.composite);
+    //     }
+    //   } />
+    // );
     return (
-      <AppExample/>
+      <App />
     );
   }
 
+  settings: ISettingRegistry.ISettings;
+  // initState: (setter: ((prefs: IPreferences) => void)) => void;
 }
 
