4471a65252ebee1fef114423d816884c183a9fcb
Sumit Jha <7sumitjha@gmail.com>
2023-12-27 13:28:34 +0000

    upgrade to jupyterlab 4 and fix tests

--- ui-tests/tests/jupyterlab_conda_store.spec.ts
+++ ui-tests/tests/jupyterlab_conda_store.spec.ts
@@ -17,7 +17,7 @@ test('should emit an activation console message', async ({ page }) => {
 
   expect(
     logs.filter(
-      s => s === 'JupyterLab extension jupyterlab-conda-store is activated!'
+      s => s === 'JupyterLab extension conda-store is activated!'
     )
   ).toHaveLength(1);
 });
