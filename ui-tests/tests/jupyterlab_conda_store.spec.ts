8992d9e8960e5fd099313f52bf4d8bad685c5409
Sumit Jha <7sumitjha@gmail.com>
2023-12-27 14:08:47 +0000

    make ts pretty

--- ui-tests/tests/jupyterlab_conda_store.spec.ts
+++ ui-tests/tests/jupyterlab_conda_store.spec.ts
@@ -16,8 +16,6 @@ test('should emit an activation console message', async ({ page }) => {
   await page.goto();
 
   expect(
-    logs.filter(
-      s => s === 'JupyterLab extension conda-store is activated!'
-    )
+    logs.filter(s => s === 'JupyterLab extension conda-store is activated!')
   ).toHaveLength(1);
 });
